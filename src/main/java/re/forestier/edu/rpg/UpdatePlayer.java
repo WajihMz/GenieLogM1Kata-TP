3ebc375bfe6dd683a9201b307444040287b09b5c
Mz Wajih <mz.wajih@Mzs-MacBook-Pro.local>
2025-12-08 15:07:19 +0100

    refactor(UpdatePlayer): nettoyer logique majFinDeTour - supprimer point-virgule orphelin et simplifier conditions

--- src/main/java/re/forestier/edu/rpg/UpdatePlayer.java
+++ src/main/java/re/forestier/edu/rpg/UpdatePlayer.java
@@ -153,16 +153,11 @@ public class UpdatePlayer {
                         }
                     }
             }
-        } else if(player.currenthealthpoints >= player.healthpoints/2){
+        } else {
             if(player.currenthealthpoints >= player.healthpoints) {
                 player.currenthealthpoints = player.healthpoints;
                 return;
             }
-        }
-
-
-        if(player.currenthealthpoints >= player.healthpoints) {
-            player.currenthealthpoints = player.healthpoints;
         }
     }
 }
