40f73bcd9e498086a34854fa159c929edd1f02a2
Mz Wajih <mz.wajih@Mzs-MacBook-Pro.local>
2025-12-11 18:04:01 +0100

    refactor: créer énumération STATS pour les statistiques

---
+++ src/main/java/re/forestier/edu/rpg/STATS.java
@@ -0,0 +1,14 @@
+package re.forestier.edu.rpg;
+
+/**
+ * Énumération représentant les différentes statistiques d'un joueur.
+ */
+public enum STATS {
+    INT,
+    DEF,
+    CHA,
+    ATK,
+    ALC,
+    VIS
+}
+
