f84ce77396087e86d14cb3b2f9cfabf5ec34708c
Mz Wajih <mz.wajih@Mzs-MacBook-Pro.local>
2025-12-11 20:11:22 +0100

    refactor: créer classe Archer héritant d'AbstractPlayer

---
+++ src/main/java/re/forestier/edu/rpg/AvatarClasses/Archer.java
@@ -0,0 +1,49 @@
+package re.forestier.edu.rpg.AvatarClasses;
+
+import re.forestier.edu.rpg.AbstractPlayer;
+import re.forestier.edu.rpg.ITEM;
+import re.forestier.edu.rpg.STATS;
+
+import java.util.ArrayList;
+
+public class Archer extends AbstractPlayer {
+
+    public Archer(String playerName, String avatarName, int maximumHealth, int money, ArrayList<String> inventory) {
+        super(playerName, avatarName, maximumHealth, money, inventory);
+        this.className = "Archer";
+        this.classDescription = "A skilled marksman with exceptional vision";
+    }
+
+    @Override
+    protected void initializeStatistics() {
+        statistics.put(STATS.INT, new Integer[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1});
+        statistics.put(STATS.ATK, new Integer[]{3, 3, 3, 3, 4, 4, 4, 4, 4, 4});
+        statistics.put(STATS.CHA, new Integer[]{1, 2, 2, 2, 2, 2, 2, 2, 2, 2});
+        statistics.put(STATS.VIS, new Integer[]{3, 3, 3, 3, 3, 3, 3, 3, 3, 3});
+        statistics.put(STATS.DEF, new Integer[]{0, 1, 1, 2, 2, 2, 2, 2, 2, 2});
+        statistics.put(STATS.ALC, new Integer[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+    }
+
+    @Override
+    public void processEndOfTurn() {
+        if (isKO()) {
+            System.out.println("Le joueur est KO !");
+            return;
+        }
+
+        if (currentHP < maximumHealth / 2) {
+            addCurrentHealthPoints(1);
+            if (inventory.contains(ITEM.MAGIC_BOW.getName())) {
+                int bonusHP = currentHP / 8 - 1;
+                if (bonusHP > 0) {
+                    addCurrentHealthPoints(bonusHP);
+                }
+            }
+        } else {
+            if (currentHP > maximumHealth) {
+                currentHP = maximumHealth;
+            }
+        }
+    }
+}
+
